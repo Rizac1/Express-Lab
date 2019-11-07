9705aecb3ab4ec5beac56405a3a6c5855fd0d245
Michael <Michael.kingery21@gmail.com>
2019-11-07 14:52:49 -0600

    commit 2

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
-// const path = require('path');
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+let app = express();
 
-app.get('/', (req, res) => res.send('Hello from the web server...'))
+// write form submissions in this file
+let filePath = path.join(__dirname, './formsubmissions.json');
 
+// respond with simple text on root
+// app.get('/', (req, res) => {                
+//     res.send('Hello from the web server side ...');           
+// })
+
+// log url of each root request
 app.use((req, res, next) => {
-    console.log(req.url);
+    console.log(req.originalUrl);
     next();
 })
 
-// app.use(express.static(path.join(__dirname, '../public')));
+// use body-parser for form
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('public'))
+// write form submissions to json file when posted; respond with simple text
+app.post('/contact-form', (req, res) => {
+    fs.appendFileSync(filePath, `{"name": "${req.body.name}", "email": "${req.body.email}"}\n`);
+    res.send("Registration Complete");
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+// send contents of form submissions json file
+app.get('/formsubmissions', (req, res) => {
+    res.sendFile(filePath, err => console.log(err))
+})
+
+// serve up html & css files
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(3000);
